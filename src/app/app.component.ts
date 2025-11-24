89637ed3c5971534c3fa73115ce26f544cb37b77
Esdras <esdras.diogo@fagrontech.com.br>
2025-11-24 03:10:22 -0300

    Gerenciamento de Gastos primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatCardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { ConsultaComponent } from './views/consulta/consulta.component';
+import { DetalhesComponent } from './views/detalhes/detalhes.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'consulta', component: ConsultaComponent
+  },
+  {
+    path: 'detalhes', component: ConsultaComponent
+  },
+  {
+    path: '**', component: ConsultaComponent
+  }
+];
